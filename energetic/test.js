eb8f78a8f6dbdf68d197873b865ac274af27b082
Akshara Debnath <70068533+akshxrx@users.noreply.github.com>
2021-06-22 19:41:48 -0400

    revert last commit

--- energetic/test.js
+++ energetic/test.js
@@ -24,16 +24,8 @@ setInterval(function() {
 }, 1000);
 
 
-const countEl = document.getElementById('count');
+var n = localStorage.getItem('on_load_counter');
 
-updateVisitCount();
 
-function updateVisitCount() {
-	fetch('https://api.countapi.xyz/update/florin-popcom/codepen/?amount=1')
-	.then(res => res.json())
-	.then(res => {
-		countEl.innerHTML = res.value;
-	})
-}
 
 
