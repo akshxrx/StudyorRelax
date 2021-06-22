8990d795267342422f58fb31a6c1815e41fdc403
Akshara Debnath <70068533+akshxrx@users.noreply.github.com>
2021-06-22 19:34:32 -0400

    web counter

--- energetic/test.js
+++ energetic/test.js
@@ -24,5 +24,16 @@ setInterval(function() {
 }, 1000);
 
 
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/florin-popcom/codepen/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
 
 
