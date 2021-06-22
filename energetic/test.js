aac954a6d3ce2600a979f08abbaa3f2e3bdbcb88
Akshara Debnath <70068533+akshxrx@users.noreply.github.com>
2021-06-22 19:04:05 -0400

    first commit

---
+++ energetic/test.js
@@ -0,0 +1,28 @@
+// function openNav() {
+//     document.getElementById("mainSidepanel").style.width = "1300px";
+// }
+
+// function closeNav() {
+//     document.getElementById("mainSidepanel").style.width = "0";
+// }
+
+function openNavbuttons() {
+    document.getElementById("navButtons").style.width = "20em";
+
+}
+
+function closeNavbuttons() {
+    document.getElementById("navButtons").style.width = "0";
+}
+
+var sec = 0;
+
+function pad(val) { return val > 9 ? val : "0" + val; }
+setInterval(function() {
+    document.getElementById("seconds").innerHTML = pad(++sec % 60);
+    document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
+}, 1000);
+
+
+
+
